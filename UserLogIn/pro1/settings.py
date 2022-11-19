c318455f2bf92ded3bac16aab885d590b8c382b8
RitiKSiBJr <letter.ritik060@gmail.com>
2022-11-19 13:20:14 +0545

    UserLogIn

--- UserLogIn/pro1/settings.py
+++ UserLogIn/pro1/settings.py
@@ -20,7 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2gw$y5@we4y*!z7_uf#-(a0&45fe)b*7@r%f*_*ar6-1(q=r!&'
+from environ import Env
+
+env = Env()
+env.read_env()
+SECRET_KEY = 'MY_SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
