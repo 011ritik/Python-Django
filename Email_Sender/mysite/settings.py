b81d7d616224d4280ef9ebe3e401cd950c966822
RitiKSiBJr <letter.ritik060@gmail.com>
2022-12-21 17:51:05 +0545

    email sender

--- Email_Sender/mysite/settings.py
+++ Email_Sender/mysite/settings.py
@@ -20,6 +20,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+from environ import Env 
+
+env = Env()
+env.read_env()
+
 SECRET_KEY = 'MY_SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
